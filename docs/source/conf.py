e484ca66478cef4dbafd9260d31bd10c28feb1ef
Sermet Pekin <96650846+SermetPekin@users.noreply.github.com>
2024-04-18 10:14:35 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = ['static']
 # extensions = ['myst_parser']
 source_suffix = {
     '.rst': 'restructuredtext',
