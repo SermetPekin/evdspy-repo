01e0b3a208c64197a6431ff1de9ed7406124e877
Sermet Pekin <96650846+SermetPekin@users.noreply.github.com>
2024-07-27 17:32:21 +0300

    Update setup_folders.py

--- evdspy/EVDSlocal/initial_setup/setup_folders.py
+++ evdspy/EVDSlocal/initial_setup/setup_folders.py
@@ -1,37 +1,44 @@
-
 from evdspy.EVDSlocal.common.common_imports import *
 from evdspy.EVDSlocal.utils.utils_general import *
 from evdspy.EVDSlocal.initial.start_options import *
 from ..common.colors import *
 from evdspy.EVDSlocal.initial.start_options import default_data_folder_name
-# dirs = ("pickles", default_data_folder_name)
-dirs = ( default_data_folder_name , )
-cur_folder = Path()
 import time
+
+DefaultDirs = (default_data_folder_name,)
 
 def get_cache_folder():
     pickle_folder = Path.home() / ".cache" / "evdspy"
     return pickle_folder
+
 def create_cache_folder():
-    pickle_folder =get_cache_folder()
+    pickle_folder = get_cache_folder()
     create_directory(str(pickle_folder))
+
 def check_folders_setup_necessary():
-    for item in dirs:
-        folder_ = cur_folder.absolute() / item
-        if not folder_.is_dir():
-            return True
-    pickle_folder =get_cache_folder()
-    
-    return pickle_folder.is_dir()
+    dirs = [Path(".") / x for x in DefaultDirs]
+    pickle_folder = get_cache_folder()
+    dirs.append(pickle_folder)
+    return not all([x.is_dir() for x in dirs])
+
 def check_setup():
     return not check_folders_setup_necessary()
+
 def setup_folders():
-    
     if not check_folders_setup_necessary():
         return
-    create_cache_folder()
+    dirs = [Path() / x for x in DefaultDirs]
+    pickle_folder = get_cache_folder()
+    dirs.append(pickle_folder)
+    NEWLINE = "\n"
+    def str_(x: Path):
+        return str(x.absolute())
+    content = NEWLINE.join(tuple(map(str_, dirs)))
+    print_with_success_style(f"""
+    Folders will bre created
+    ..............................
+    {content}
+""")
     for item in dirs:
-        folder_ = cur_folder.absolute() / item
-        print_with_creating_style(f"Creating directory...{folder_}")
-        create_directory(str(folder_))
-        time.sleep(0.8)
+        create_directory(str(item))
+        print_with_creating_style(f"Creating directory...{item}" )
