c7f9fbae4f7168983358575b2999d32b6b9648ed
sermet.pekin <Sermet.Pekin@gmail.com>
2024-08-02 13:37:29 +0300

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-html_build_dir = '../docs'
-# html_build_dir = 'docs'
+html_build_dir = '../docs'  # Directory for HTML build output
 
 project = 'evdspy'
 copyright = '2024, Sermet Pekin'
@@ -17,9 +16,9 @@ author = 'Sermet Pekin'
 version = ''
 # The full version, including alpha/beta/rc tags
 release = ''
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -30,20 +29,24 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.inheritance_diagram',
     'sphinx.ext.autosummary',
-    # 'myst_parser'
+    # 'myst_parser'  # Uncomment if you use MyST markdown
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+doctest_global_setup = """
+# Any global setup code for doctests
+"""
+
+# Disable execution of doctest blocks
+doctest_test_doctest_blocks = 'false'
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinx_book_theme'
-
-html_static_path = ['static']
+html_theme = 'sphinx_book_theme'  # Theme for HTML output
+html_static_path = ['_static']  # Static files path
 
 source_suffix = {
     '.rst': 'restructuredtext',
@@ -52,18 +55,16 @@ source_suffix = {
 }
 
 from sphinx.util import logging
-
 logger = logging.getLogger(__name__)
 
-
-
 def linkcode_resolve(domain, info):
+    """
+    Resolve the link to the source code for documentation.
+    """
     print(domain, info, "Domain info ")
     if domain != 'py':
         return None
     if not info['module']:
         return None
     filename = info['module'].replace('.', '/')
-    return "https://github.com/SermetPekin/evdspy-repo/%s.py" % filename
-
-
+    return f"https://github.com/SermetPekin/evdspy-repo/{filename}.py"
