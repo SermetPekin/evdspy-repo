bcfee146f1693de813f7464e0e4cc889f779ba88
Sermet Pekin <96650846+SermetPekin@users.noreply.github.com>
2026-04-08 14:07:35 +0300

    Update Data_processor.py

--- evdspy/EVDSlocal/index_requests/user_requests/Data_processor.py
+++ evdspy/EVDSlocal/index_requests/user_requests/Data_processor.py
@@ -1,22 +1,43 @@
 # ....................................................................... DataProcessor
 import traceback
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 import pandas as pd
 
 from evdspy.EVDSlocal.index_requests.index_util_funcs import make_df_float, json_to_df
 from evdspy.EVDSlocal.index_requests.user_requests.User_req_typings import T_maybeDf
+from evdspy.EVDSlocal.index_requests.index_util_funcs import try_date
+
+if TYPE_CHECKING:
+
+    from evdspy.EVDSlocal.index_requests.user_requests import RequestConfig
 
 
 class DataProcessor:
-    def __init__(self, data: Any):
+    def __init__(self, data: Any, config: "RequestConfig"):
         self.data = data
+        self.config = config
 
     def set_index(self, df: pd.DataFrame) -> pd.DataFrame:
         if "Tarih" in df.columns:
+            df["Tarih_string"] = df["Tarih"]
             df.set_index("Tarih", inplace=True)
         return df
-    
+
+    def fix_column_names(self, df: pd.DataFrame):
+        def fix(string: str):
+            if "tarih" in string.lower() or "week" in string.lower():
+                return string
+            return string.replace("_", ".")
+
+        df.columns = [fix(x) for x in df.columns]
+        return df
+
+    def _try_date(self, df: pd.DataFrame):
+        if df.index.name is None:
+            return df
+        return try_date(df, frequency=self.config.frequency)
+
     def process_to_dataframe(self) -> T_maybeDf:
         if self.data is False:
             return False
@@ -29,6 +50,8 @@ class DataProcessor:
         if isinstance(df, pd.DataFrame):
             df = make_df_float(df)
             df = self.set_index(df)
+            df = self.fix_column_names(df)
+            df = self._try_date(df)
         return df
 
     def __call__(self, *args, **kwargs) -> T_maybeDf:
